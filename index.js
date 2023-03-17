42c5c6255fd6feda72c9897d2c542131da11ced5
san <sansamaa0@gmail.com>
2023-03-18 02:23:49 +0300

    h.w

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const paragraph=document.querySelector('#text');
+   
+    paragraph.textContent='This is really cool!';
+ 
+});
